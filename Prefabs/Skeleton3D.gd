c0787df332664fae25e67041629f722cf1ca1876
Dimas <you@example.com>
2023-09-29 00:58:11 +0400

    initial commit

---
+++ Prefabs/Skeleton3D.gd
@@ -0,0 +1,12 @@
+extends Skeleton3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	physical_bones_start_simulation()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
